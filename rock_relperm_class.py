02f80a5a6efe7bb9fe1f018a232239407262462a
elevraadforman <elevraadforman@gmail.com>
2024-02-16 20:56:57 +0100

    next

--- rock_relperm_class.py
+++ rock_relperm_class.py
@@ -3,17 +3,16 @@ import matplotlib.pyplot as plt
 
 
 class LET:
-    def __init__(self, a, b, Lw, Ew, Tw, Lo, Eo, To):
+    def __init__(self, Lw, Ew, Tw, Lo, Eo, To, rock_type):
         
         #define paramters
-        self.a = a
-        self.b = b
-        self.Lo = Lo
-        self.Eo = Eo
-        self.To = To
-        self.Lw = Lw
-        self.Ew = Ew
-        self.Tw = Tw       
+        self.Lo =  Lo
+        self.Eo =  Eo
+        self.To =  To
+        self.Lw =  Lw
+        self.Ew =  Ew
+        self.Tw =  Tw  
+        self.rock_type = rock_type     
 
         self.Swir = 0.1
         self.Sorw = 0.3
@@ -40,22 +39,60 @@ class LET:
         ax.plot(self.Sw, self.krw, color='b')
         ax.plot(self.Sw, self.kro, color='r')
 
+        ax.set_title(self.rock_type)
+
         plt.show()
 
-class ObjectA:
-    def __init__(self):
+
+
+
+
+class Rock:
+    """
+    A class representing a rock-type.
+
+    Attributes(properties):
+    -----------
+    rock_type: string
+        the type of rock 
+    
+    porosity: float
+
+    permeability: int
+    """
+
+    def __init__(self, rock_type: str, porosity: float, permeability: int):
         self.LET = None
+        self.rock_type = rock_type
+        self.porosity = porosity
+        self.permeability = permeability
+        print('A new object of class Rock is created')
 
-    def add_LET(self, a, b, Lw, Ew, Tw, Lo, Eo, To):
-        self.LET = LET(a, b, Lw, Ew, Tw, Lo, Eo, To)
+    def print_properties(self):
+        """
+        A method to print the properties of a rock.
+        """
+        print(f"Rock type: {self.rock_type}, Porosity: {self.porosity}, Permeability: {self.permeability}mD")
 
-# Creating an instance of ObjectA
-objectA = ObjectA()
+    def add_LET(self, Lw, Ew, Tw, Lo, Eo, To):
+        self.LET = LET( Lw, Ew, Tw, Lo, Eo, To, self.rock_type)
 
-# Adding LET to objectA at the same time creating an instance of LET
-objectA.add_LET(13, 24, 2, 3, 4, 2, 3, 4)
 
-print(objectA.LET.Swir)
+# Creating an instances of rock
+Berea = Rock("sandstone", 0.21, 400)
+Austin_chalk= Rock("chalk", 0.38, 50)
+Bachalau_carbonate = Rock("carbonate", 0.29, 1000)
+
+
+
+# Adding LET to instance of Rock at the same time creating an instance of LET
+Berea.add_LET(2, 3, 4, 2, 3, 4)
+
+
+
+print(Berea.LET.Swir)
+
+Berea.print_properties()
 
 # Calling method plotLET of LET through objectA
-objectA.LET.plotLET()
+Berea.LET.plotLET()
